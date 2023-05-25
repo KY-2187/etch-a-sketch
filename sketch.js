a3698cdca67ce6228b6ca7a9a698d2d1a77d107b
KY-2187 <kyrahung1@gmail.com>
2023-05-25 12:38:10 +0800

    Add drawing function

--- sketch.js
+++ sketch.js
@@ -1,9 +1,27 @@
 const grid = document.querySelector('#grid');
 
+let isMouseDown = false;
+
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         let cell = document.createElement('div');
         cell.classList.add('cell');
         grid.appendChild(cell);
+
+        cell.addEventListener('mousedown', function(e) { 
+            e.preventDefault();
+            isMouseDown = true;
+            cell.style.backgroundColor = '#353535'; 
+        });
+
+        cell.addEventListener('mousemove', function() {
+            if (isMouseDown) {
+                cell.style.backgroundColor = '#353535';
+            }
+        });
+    
+        cell.addEventListener('mouseup', function() {
+            isMouseDown = false;
+        });
     }
 }
